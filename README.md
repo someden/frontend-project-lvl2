ec779ae2d511a54c7a72ecc5c6bfef6074a7f9cf
Denis <someden@ya.ru>
2022-03-27 12:50:29 +0300

    step 3

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/someden/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/someden/frontend-project-lvl2/actions)
+
+### Setup
+
+```sh
+make install
+```
+
+### Usage
+
+```sh
+gendiff fixtures/file1.json fixtures/file2.json
+```
+
+[![asciicast](https://asciinema.org/a/SirtRH5MlD3TXinCYwFbUSi3n.svg)](https://asciinema.org/a/SirtRH5MlD3TXinCYwFbUSi3n)
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
+import { Command } from 'commander';
+import genDiff from '../index.js';
+
 const program = new Command();
 
 program
@@ -8,6 +10,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
